4ed7be0571fdf5947a44d3d512c1f696ea2daa0b
Bryce Liu <bryce.liu@zaisgroup.com>
2022-07-13 16:39:24 +0000

    Add pay out function, test cases

--- contracts/BettingNumber.sol
+++ contracts/BettingNumber.sol
@@ -6,29 +6,37 @@ import "hardhat/console.sol";
 
 contract BettingNumber {
     
-    uint256 private numberToGuess;
+    int256 private numberToGuess;
     uint256 pot;
     address private _owner;
+    uint256 MAX_INT;
 
     event NewBet(address indexed from, uint256 betAmount);
 
     struct Bet {
-        address better;
+        address payable better;
         uint256 betAmount;
+        int256 guess;
     }
 
     Bet[] bets;
+    Bet[] closestBet;
 
     constructor() payable {
-        console.log("Hello my name is Bryce and I'm betting :0");
+        console.log("Hello my name is Bryce and I'm the casino owner :0");
+        console.log("Must guess a positive integer.");
         numberToGuess = 81;
         _owner = msg.sender;
+        MAX_INT = 2**256 - 1;
+        pot = 0 ether;
     }
 
-    function bet() external payable {
+    function bet(int256 guess) external payable {
         //Create bet
-        Bet memory betMade = Bet(msg.sender, msg.value);
+        Bet memory betMade = Bet(payable(msg.sender), msg.value, guess);
         bets.push(betMade);
+        console.log("Adding to pot: ", msg.value);
+        pot += msg.value;
     }
 
     function getBets() public view returns (Bet[] memory) {
@@ -36,13 +44,77 @@ contract BettingNumber {
         return bets;
     }
 
-    // function payOut() external onlyOwner {
-    //     address payable _winner = ;
-    //     _winner.transfer(_value);
-    // }
-
-    modifier onlyOwner {
-        require(msg.sender == _owner);
-        _;
+    function getPot() public view returns (uint256) {
+        console.log("Pot size: ", pot);
+        return pot;
     }
+
+    function abs(int x) private pure returns (int) {
+        return x>=0 ? x : -x;
+    }
+
+    function payOut() external payable {
+        require(msg.sender == _owner);
+        
+        // iterate through bets to find closest bet
+        for(uint i=0; i<bets.length; i++){
+            if(closestBet.length>0) {
+                // console.log("Closest better for now:", closestBet[0].better, "Guess: ", uint256(closestBet[0].guess));
+                // int error = abs(bets[i].guess - numberToGuess);
+                // int topGuessError = abs(closestBet[0].guess - numberToGuess);
+                // console.log("Error of guess:", uint256(error));
+                // console.log("Top guess right now:", uint256(topGuessError));
+                if(abs(bets[i].guess - numberToGuess) == abs(closestBet[0].guess - numberToGuess)) {
+                    closestBet.push(bets[i]);
+                    console.log("Another winner: ", bets[i].better);
+                }
+                if(abs(bets[i].guess - numberToGuess) < abs(closestBet[0].guess - numberToGuess)) {
+                    delete closestBet;
+                    closestBet.push(bets[i]);
+                    console.log("New winner: ", bets[i].better);
+                }
+                else {
+                    console.log("This person didn't win:", bets[i].better);
+                }
+            }
+            else {
+                closestBet.push(bets[i]);
+                console.log("New winner for now...: ", bets[i].better);
+            }
+            
+        }
+        console.log("Current closestBet size:", closestBet.length);
+        // if there's only 1 winner, payout to winner
+        if(closestBet.length == 1){
+            console.log("Only one winner!");
+            closestBet[0].better.transfer(pot);
+            console.log(closestBet[0].better, "is the winner!");
+            console.log("Pay out done.");
+        }
+        
+        // if there's no winner, payout to no one
+        if(closestBet.length == 0){
+            console.log("No winner, no payout!");
+        }
+
+        // if there's multiple winners, split up the pot
+
+        if(closestBet.length > 1){
+            console.log("Multiple winners, split the pot!");
+            uint256 splitPot = pot/closestBet.length;
+            console.log("Pay out to winners:", splitPot);
+            for(uint i=0; i<closestBet.length; i++){
+                closestBet[i].better.transfer(splitPot);
+                console.log(closestBet[i].better, "is a winner!");
+            }
+            console.log("Pay out done.");
+        }
+        pot = 0;
+        // console.log("Pot size now:", pot);
+        delete bets;
+        // console.log("Bets reset. Size: ", bets.length);
+        delete closestBet;
+        // console.log("Closest bets reset. Size: ", closestBet.length);
+    }
+
 }
--- scripts/run.js
+++ scripts/run.js
@@ -2,21 +2,54 @@ const { hexStripZeros } = require("ethers/lib/utils");
 const { ethers } = require("hardhat");
 
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
+    const [owner, better1, better2] = await hre.ethers.getSigners();
     const BetContractFactory = await hre.ethers.getContractFactory("BettingNumber");
     const BetContract = await BetContractFactory.deploy();
     await BetContract.deployed();
 
     console.log("Contract deployed to:", BetContract.address);
     console.log("Contract deployed by:", owner.address);
+    console.log("Soon to be betters:", better1.address, better2.address);
 
+    // Initial betCount should be 0, pot size 0
     let betCount;
     betCount = await BetContract.getBets();
+    let potSize;
+    potSize = await BetContract.getPot();
 
-    let betTxn = await BetContract.bet({value: ethers.utils.parseEther("0.5")});
+    // Make bets of 0.5 by Ox70 on number 79, 1.2 by 0x3C on number 94
+    let betTxn = await BetContract.connect(better1).bet(79, {value: ethers.utils.parseEther("0.5")});
     await betTxn.wait();
 
+    let betTxn2 = await BetContract.connect(better2).bet(94, {value: ethers.utils.parseEther("1.2")});
+    await betTxn2.wait();
+    // Note: 0x70 should be winner
+
+    // Check if betCount and potSize have right amounts
     betCount = await BetContract.getBets();
+    potSize = await BetContract.getPot();
+
+    // Check if potSize matches contract balance
+    contractBalance = await hre.ethers.provider.getBalance(BetContract.address);
+    console.log(
+        "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
+
+    // Pay out the potSize to the closest winner
+    let payOutTxn = await BetContract.connect(owner).payOut();
+    await payOutTxn.wait();
+
+    // Check if betCount and potSize have reset to 0
+    betCount = await BetContract.getBets();
+    potSize = await BetContract.getPot();
+
+    // Check if potSize matches contract balance
+    contractBalance = await hre.ethers.provider.getBalance(BetContract.address);
+    console.log(
+        "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
 };
 
 const runMain = async () => {
